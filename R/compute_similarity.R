01a2a1d29948bbb04d78fd00dd1cd20d721b6b67
Shantanu Singh <shsingh@wmc94-e26.broadinstitute.org>
2015-06-25 21:52:33 -0400

    Added real code for computing similarity

--- R/compute_similarity.R
+++ R/compute_similarity.R
@@ -2,14 +2,23 @@
 #'
 #' Function to measure similarity between two vectors
 #'
-#' @param x vector
-#' @param y vector of the same length as x
+#' @param D1 data.frame
+#' @param D2 same dimensions as \code{D1}
 #'
-#' @return Similarity between x and y
+#' @return Similarity between D1 and D2
 #'
-#' @examples
-#' compute_similarity(c(1,2,3,4,5), c(1,2,4,3,5))
-compute_similarity <- function(x, y) {
-  cor(x, y)
+##' @examples
+##' compute_similarity(c(1,2,3,4,5), c(1,2,4,3,5))
+#'
+compute_similarity <- function(D1,
+                               D2,
+                               grouping_cols = NULL,
+                               method = "spearman") {
+
+  D1_mat <- as.matrix(D1[,setdiff(names(D1), grouping_cols)])
+  D2_mat <- as.matrix(D2[,setdiff(names(D2), grouping_cols)])
+
+  cor(t(D1_mat), t(D2_mat))
+
 }
 
--- tests/testthat/test-compute_similarity.R
+++ tests/testthat/test-compute_similarity.R
@@ -1,8 +1,25 @@
 context("Test functions that measure similarity between vectors")
 
-x <- seq(10)
-y <- c(seq(3), seq(7))
 
-test_that("Similarity values are valid", {
-  expect_true(compute_similarity(x, y) <= 1)
+library(dplyr)
+cpseedseq_pruned <-
+  cpseedseq %>%
+  dplyr::filter(MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
+                  c("HDAC1", "HDAC2", "HDAC7"))
+hdac1 <-
+  cpseedseq_pruned %>%
+  dplyr::filter(MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
+                  c("HDAC1"))
+hdac2 <-
+  cpseedseq_pruned %>%
+  dplyr::filter(MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
+                  c("HDAC2"))
+
+metadata_cols <- stringr::str_subset(names(cpseedseq),
+                                     "MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_")
+test_that("Similarity matrix for test dataset is valid", {
+  expect_true(
+    all(dim(compute_similarity(hdac1, hdac2, grouping_cols = metadata_cols)) == c(33,33))
+    )
+
 })
