a051216349ded101bdafd442525f6bbfe1208d8e
Shantanu Singh <shsingh@broadinstitute.org>
2015-06-29 16:27:00 -0400

    Removed dplyr functions

--- tests/testthat/test-compute_similarity.R
+++ tests/testthat/test-compute_similarity.R
@@ -1,25 +1,27 @@
-context("Test functions that measure similarity between vectors")
+context("Measure similarity between vectors")
 
 
-library(dplyr)
+# Create a smaller version of the cpseedseq dataset
 cpseedseq_pruned <-
-  cpseedseq %>%
-  dplyr::filter(MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
-                  c("HDAC1", "HDAC2", "HDAC7"))
+  with(cpseedseq,
+       cpseedseq[MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
+                  c("HDAC1", "HDAC2", "HDAC7"),])
 hdac1 <-
-  cpseedseq_pruned %>%
-  dplyr::filter(MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
-                  c("HDAC1"))
+  with(cpseedseq_pruned,
+       cpseedseq_pruned[MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
+                          c("HDAC1"),])
 hdac2 <-
-  cpseedseq_pruned %>%
-  dplyr::filter(MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
-                  c("HDAC2"))
+  with(cpseedseq_pruned,
+       cpseedseq_pruned[MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_GeneSymbol %in%
+                          c("HDAC2"),])
 
 metadata_cols <- stringr::str_subset(names(cpseedseq),
                                      "MultipleHairpin_2013_03_07_Analysis_Per_Image.Image_Metadata_")
+cmat <- compute_similarity(hdac1, hdac2, grouping_cols = metadata_cols)
+
 test_that("Similarity matrix for test dataset is valid", {
   expect_true(
-    all(dim(compute_similarity(hdac1, hdac2, grouping_cols = metadata_cols)) == c(33,33))
+    all(dim(cmat) == c(NROW(hdac1),NROW(hdac2)))
     )
 
 })
