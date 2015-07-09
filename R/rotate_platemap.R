20b74960ca9f86000e35db4a4bf54e42c9f665bf
Shantanu Singh <shsingh@broadinstitute.org>
2015-07-09 17:26:51 -0400

    Fixed bug in rotate_platemap

--- R/rotate_platemap.R
+++ R/rotate_platemap.R
@@ -1,5 +1,6 @@
 #' Rotate the platemap for a 384-well plate
 #'
+#' @param ... Arguments to be passed to methods
 #' @return profile.data with platemap rotate by 180 degrees
 #'
 rotate_platemap <- function(...) UseMethod("rotate_platemap")
@@ -9,12 +10,15 @@ rotate_platemap <- function(...) UseMethod("rotate_platemap")
 #' @param P profile.data generated from a 384-well plate experiment
 #' and containing 'Well' in the metdata
 #'
+#' @importFrom dplyr '%>%'
+
 rotate_platemap.profile.data <- function(P, ...) {
   testthat::expect_false("WellRow" %in% names(meta(P)))
   testthat::expect_false("WellCol" %in% names(meta(P)))
   testthat::expect_true("Well" %in% names(meta(P)))
 
-  rotmap <- outer(letters[1:16], seq(24) %>% sprintf("%02d", .), FUN="paste", sep="")
+  rotmap <- outer(letters[1:16], sprintf("%02d", seq(24)),
+                  FUN = "paste", sep = "")
   names(rotmap) <- as.character(rotmap[rev(seq(16)), rev(seq(24))])
 
   testthat::expect_true(all(meta(P)$Well %in% rotmap))
