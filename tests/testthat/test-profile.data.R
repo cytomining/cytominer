ba85f84d33288fe099a61d539d2f2b141b6be6d1
Shantanu Singh <shsingh@broadinstitute.org>
2015-07-09 13:50:25 -0400

    Updated tests for profile.data

--- tests/testthat/test-profile.data.R
+++ tests/testthat/test-profile.data.R
@@ -1,18 +1,26 @@
-context("Load profile data")
+context("Load and do some basic processing on a profile.data object")
 
+cfg_fname <-
+  system.file("extdata",
+              "well-summary-profile_mean-median-robust_std-untreated_norm.yml",
+              package = "pertminr")
 
 test_that("Sample dataset loads correctly", {
   skip("Skipping because it is too slow")
-  expect_is(
-    profile.data("../../inst/extdata/well-summary-profile_mean-median-robust_std-untreated_norm.yml",
-                 use_csv = TRUE),
-    "profile.data")
+  expect_is(profile.data(cfg_fname, use_csv = TRUE),
+            "profile.data")
 })
 
-D <- profile.data("../../inst/extdata/well-summary-profile_mean-median-robust_std-untreated_norm.yml")
+P <- profile.data(cfg_fname)
 
 test_that("Sample dataset has expected structure", {
-  expect_is(D$metadata, "data.frame")
-  expect_is(D$featdata, "data.frame")
+  expect_is(meta(P), "data.frame")
+  expect_is(feat(P), "data.frame")
+})
 
+test_that("Processing metadata has expected behavior", {
+  expect_is(
+    process_metadata(P, strip_cellprofiler_db_tags = T),
+    "profile.data"
+  )
 })
