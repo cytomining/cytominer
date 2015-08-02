be7de65272fcc50eef479bf483fcb4d581dc57a1
Shantanu Singh <shsingh@broadinstitute.org>
2015-08-02 14:27:50 -0400

    rename argument

--- R/add_metadata.R
+++ R/add_metadata.R
@@ -2,7 +2,7 @@
 #'
 #' @param P profile.data
 #' @param metadata data.frame with new metadata columns
-#' @param join_columns columns on which to join
+#' @param join_cols columns on which to join
 #' @param ... additional parameters
 
 add_metadata <- function(P, ...)
@@ -11,15 +11,15 @@ add_metadata <- function(P, ...)
 
 #' @describeIn add_metadata Add metadata to profile.data object
 
-add_metadata.profile.data <- function(P, metadata, join_columns, ...) {
+add_metadata.profile.data <- function(P, metadata, join_cols, ...) {
   testthat::expect_is(P, "profile.data")
-  testthat::expect_true(all(join_columns %in% names(metadata)))
-  testthat::expect_true(all(join_columns %in% names(meta(P))))
+  testthat::expect_true(all(join_cols %in% names(metadata)))
+  testthat::expect_true(all(join_cols %in% names(meta(P))))
   common_columns <- setdiff(intersect(names(meta(P)), names(metadata)),
-                            join_columns)
+                            join_cols)
   testthat::expect_equal(length(common_columns), 0)
 
-  metadata_1 <- dplyr::left_join(meta(P), metadata, by = join_columns)
+  metadata_1 <- dplyr::left_join(meta(P), metadata, by = join_cols)
   testthat::expect_equal(nrow(metadata_1), nrow(meta(P)))
   testthat::expect_true(all(names(meta(P)) %in% names(metadata_1)),
                         info = setdiff(names(meta(P)), names(metadata_1)))
--- tests/testthat/test-add_metadata.R
+++ tests/testthat/test-add_metadata.R
@@ -11,11 +11,11 @@ metadata %<>% dplyr::rename(shRNA_CloneID = Clone.ID)
 test_that("Adding new metadata works as expected", {
   expect_is(add_metadata(cpseedseq_prf,
                          metadata,
-                         join_columns = "shRNA_CloneID"), "profile.data")
+                         join_cols = "shRNA_CloneID"), "profile.data")
 
   cpseedseq_prf_1 <- add_metadata(cpseedseq_prf,
                                   metadata,
-                                  join_columns = "shRNA_CloneID")
+                                  join_cols = "shRNA_CloneID")
 
   expect_true(all(names(metadata) %in% names(meta(cpseedseq_prf_1))))
   expect_equal(nrow(meta(cpseedseq_prf)), nrow(meta(cpseedseq_prf_1)))
