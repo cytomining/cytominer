20d8e665b5f4faac91bdfe80e4a73d824a5f7393
Shantanu Singh <shsingh@broadinstitute.org>
2015-08-04 14:53:55 -0400

    Update data after updating process_metadata

--- data-raw/cpseedseq_prf_generate.R
+++ data-raw/cpseedseq_prf_generate.R
@@ -1,13 +1,13 @@
 # Load cpseedseq dataset as a profile.data object
 cfg_fname <- "inst/extdata/well-summary-profile_mean-median-robust_std-untreated_norm.yml"
-cpseedseq_prf <- pertminr::profile.data(cfg_fname)
+cpseedseq_prf <- pertminr:::profile.data(cfg_fname)
 
-cpseedseq_prf <- pertminr::process_metadata(cpseedseq_prf,
+cpseedseq_prf <- pertminr:::process_metadata(cpseedseq_prf,
   strip_cellprofiler_db_tags = T)
 
 cpseedseq_prf$metadata <- dplyr::filter(cpseedseq_prf$metadata,
                                         Plate %in% c(38034, 38003, 37983))
 
-cpseedseq_prf <- post_filter_metadata(cpseedseq_prf)
+cpseedseq_prf <- pertminr:::post_filter_metadata(cpseedseq_prf)
 
 devtools::use_data(cpseedseq_prf)
