18b12e77fdb59db94df7db980e8bff87f55f2aba
Shantanu Singh <shsingh@broadinstitute.org>
2016-02-22 13:49:57 -0500

    Update fixture

--- tests/testthat/test-normalize.R
+++ tests/testthat/test-normalize.R
@@ -1,4 +1,8 @@
 test_that("Normalizing intensity is valid", {
   expect_equal(nrow(fixture_intensities), 4959)
-  expect_equal(nrow(fixture_normalized_intensities), 4959)
+  normalized <- normalize(fixture_intensities,
+                          fixture_intensities %>%
+                            dplyr::filter(well_description %in%
+                                            c("A01", "A02")))
+
 })
