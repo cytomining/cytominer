e0e714ab1a413310f5e0ef5139e72fc57e88aaf2
Shantanu Singh <shsingh@broadinstitute.org>
2016-03-01 17:09:27 -0500

    Default value for operation

--- R/select.R
+++ R/select.R
@@ -8,7 +8,7 @@
 #' @return data.frame after feature selection
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
-select <- function(population, variables, operation, ...) {
+select <- function(population, variables, operation = "variance_threshold", ...) {
   if (operation == "variance_threshold") {
     variance_threshold(population, variables, ...)
   } else if (operation == "correlation_threshold") {
