0693d9a2d79f0c765b594fc576408945369f00e0
Shantanu Singh <shsingh@broadinstitute.org>
2016-03-01 17:01:00 -0500

    Cleaned up integration test

--- tests/testthat/test-cytominr.R
+++ tests/testthat/test-cytominr.R
@@ -1,54 +1,62 @@
 test_that("cytominr", {
+
   fixture <-
     system.file("extdata", "fixture_intensities.sqlite", package = "cytominr")
 
   measurements <-
     dplyr::tbl(src = dplyr::src_sqlite(path = fixture), "measurements")
 
-  features <- c(
-    "integrated",
-    "maximum",
-    "mean",
-    "median",
-    "median_absolute_deviation",
-    "minimum",
-    "standard_deviation",
-    "third_quartile"
-  )
+  features <-
+    c(
+      "integrated",
+      "maximum",
+      "mean",
+      "median",
+      "median_absolute_deviation",
+      "minimum",
+      "standard_deviation",
+      "third_quartile"
+    )
 
-  normalized <- normalize(
-    population = measurements %>%
-      dplyr::filter(
-        well_description %in% c(
-          "A01",
-          "A02",
-          "A03",
-          "A04"
+  normalized <-
+    normalize(
+      population = measurements %>%
+        dplyr::filter(
+          well_description %in% c(
+            "A01",
+            "A02",
+            "A03",
+            "A04"
+          )
+        ) %>%
+        dplyr::select_(
+          .dots = features
+        ),
+      variables = features,
+      sample = measurements %>%
+        dplyr::filter(
+          well_description %in% c(
+            "A01",
+            "A02"
+          )
+        ) %>%
+        dplyr::select_(
+          .dots = features
         )
-      ) %>%
-      dplyr::select_(
-        .dots = features
-      ),
+    )
+
+  transformed <-
+    transform(
+      population = normalized,
+      variables = features
+    )
+
+  selected <- select(
+    population = transformed %>% dplyr::collect(),
     variables = features,
-    sample = measurements %>%
-      dplyr::filter(
-        well_description %in% c(
-          "A01",
-          "A02"
-        )
-      ) %>%
-      dplyr::select_(
-        .dots = features
-      ),
-    operation = "standardize"
+    sample = transformed %>% dplyr::collect(),
+    operation = "variance_threshold"
   )
 
-  transformed <- transform(
-    population = normalized,
-    variables = features,
-    operation = "generalized_log",
-    c = 1
-  )
-
-  expect_equal(transformed %>% dplyr::collect() %>% nrow(), 44631)
+  expect_equal(selected %>% dplyr::collect() %>% nrow(), 44631)
 })
