054ab229c9285b65eccf55bf2fd906abd463e9d1
Shantanu Singh <shsingh@broadinstitute.org>
2016-03-07 18:13:32 -0500

    Shortened for variance_threshold

--- tests/testthat/test-variance_threshold.R
+++ tests/testthat/test-variance_threshold.R
@@ -1,27 +1,19 @@
-test_that("variance thresholded shapes is valid", {
+test_that("variance threshold is valid", {
 
-  metadata <- c("plate_barcode",
-                "well_description",
-                "image_description",
-                "object_description",
-                "pattern_description")
+  dat <- data.frame(x = rnorm(30), y = 1)
 
-  features <- c("AreaShape_euler_number",
-                 "AreaShape_compactness")
+  expect_equal(
+    variance_threshold(population = dat,
+                       variables = c('x', 'y'),
+                       sample = dat),
+    c("y")
+  )
 
-  a <- setdiff(colnames(fixture_shapes),
-               variance_threshold(population = fixture_shapes,
-                                  variables = features,
-                                  sample = fixture_shapes))
-
-  b <- fixture_variance_threshold_shapes %>% names()
-
-  expect_equal(a, b)
-
-  a <- variance_threshold(population = fixture_shapes,
-                          variables = c("AreaShape_compactness"),
-                          sample = fixture_shapes)
-
-  expect_equal(length(a), 0)
+  expect_equal(
+    variance_threshold(population = dat,
+                       variables = c('x'),
+                       sample = dat),
+    character(0)
+  )
 
 })
