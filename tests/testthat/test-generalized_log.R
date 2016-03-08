ed999e39e7ebee7fd10d22221f4ed7fca0fb71ce
Shantanu Singh <shsingh@broadinstitute.org>
2016-03-08 08:50:40 -0500

    Change test to using sqlite

--- tests/testthat/test-generalized_log.R
+++ tests/testthat/test-generalized_log.R
@@ -1,18 +1,21 @@
-test_that("generalized log is valid", {
+test_that("generalized log works with sqlite", {
 
   dat <- data.frame(x = rnorm(5), y = rnorm(5))
+
+  dat <- dplyr::copy_to(dplyr::src_sqlite(":memory:", create = T),
+                        dat)
 
   glog <- function(x, c=1) log( (x + ( x ^ 2 + c ^ 2) ^ 0.5 ) / 2 )
 
   expect_equal(
     generalized_log(population = dat,
-                    variables = c('x', 'y')),
-    glog(dat)
+                    variables = c('x', 'y')) %>% dplyr::collect(),
+    glog(dat %>% dplyr::collect())
   )
 
   expect_equal(
     generalized_log(population = dat,
-                    variables = c('x')),
-    within(dat, x <- glog(x))
+                    variables = c('x')) %>% dplyr::collect(),
+    within(dat %>% dplyr::collect(), x <- glog(x))
   )
 })
