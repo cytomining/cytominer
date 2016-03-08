1224b682ed05199bf3e044a7edaf4f7722ca3e20
Shantanu Singh <shsingh@broadinstitute.org>
2016-03-08 08:50:35 -0500

    Change test to using sqlite

--- tests/testthat/test-variance_threshold.R
+++ tests/testthat/test-variance_threshold.R
@@ -1,18 +1,21 @@
-test_that("variance threshold is valid", {
+test_that("variance threshold works with sqlite", {
 
   dat <- data.frame(x = rnorm(30), y = 1)
+
+  dat <- dplyr::copy_to(dplyr::src_sqlite(":memory:", create = T),
+                        dat)
 
   expect_equal(
     variance_threshold(population = dat,
                        variables = c('x', 'y'),
-                       sample = dat),
+                       sample = dat %>% dplyr::collect()),
     c("y")
   )
 
   expect_equal(
     variance_threshold(population = dat,
                        variables = c('x'),
-                       sample = dat),
+                       sample = dat %>% dplyr::collect()),
     character(0)
   )
 
