0a6c46f2ecbece29e5a2ce8841ff2c2010e06614
Shantanu Singh <shsingh@broadinstitute.org>
2016-03-08 08:50:47 -0500

    Change test to using sqlite

--- tests/testthat/test-correlation_threshold.R
+++ tests/testthat/test-correlation_threshold.R
@@ -1,23 +1,24 @@
-test_that("correlation thresholded intensities is valid", {
+test_that("correlation threshold works with sqlite", {
 
   set.seed(123)
   dat <- data.frame(x = rnorm(30))
   dat$y <- rnorm(30)/1000
   dat$z <- dat$x + rnorm(30)/1000
 
-  findCorrelation(cor(dat))
+  dat <- dplyr::copy_to(dplyr::src_sqlite(":memory:", create = T),
+                        dat)
 
   expect_equal(
     correlation_threshold(population = dat,
                           variables = c('x', 'y', 'z'),
-                          sample = dat),
+                          sample = dat %>% dplyr::collect()),
     c("z")
   )
 
   expect_equal(
     correlation_threshold(population = dat,
                           variables = c('x', 'y'),
-                          sample = dat),
+                          sample = dat %>% dplyr::collect()),
     character(0)
   )
 
