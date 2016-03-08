6accf630f858545446f57654d913bdad556d61e9
Shantanu Singh <shsingh@broadinstitute.org>
2016-03-08 17:12:16 -0500

    Cleanup

--- R/generalized_log.R
+++ R/generalized_log.R
@@ -1,35 +1,28 @@
-#' Generalized log of rows
+#' Generalized log transform data
 #'
 #' @param population population
 #' @param variables variables
 #' @param c shift
 #'
-#' @return object after transformation
+#' @return generalized log transformed data
+#'
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
 generalized_log <- function(population, variables, c = 1) {
-
   column_names <- colnames(population)
 
   for (variable in variables) {
+
+    object <- list(lazyeval::interp(~log((x + (x^2 + c^2)^0.5 ) / 2), x = as.name(variable), c = c))
+
+    name <- paste0(variable, "_")
+
     population %<>%
-      dplyr::mutate_(
-        .dots = setNames(
-          list(
-            lazyeval::interp(
-              ~ log( (x + ( x ^ 2 + c ^ 2) ^ 0.5 ) / 2 ),
-              x = as.name(variable),
-              c = c
-            )
-          ),
-          paste0(variable, "_")
-        )
-      )
+      dplyr::mutate_(.dots = setNames(object, name))
   }
 
   population %>%
     dplyr::select_(~-one_of(variables))  %>%
     dplyr::rename_(.dots = setNames(paste0(variables, "_"), variables)) %>%
     dplyr::select_(.dots = column_names)
-
 }
