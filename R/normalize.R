e92260bf203dd1658f6f706cefb34238f5b55eff
Shantanu Singh <shsingh@broadinstitute.org>
2016-08-07 01:53:34 -0400

    Document and cleanup

--- R/normalize.R
+++ R/normalize.R
@@ -11,6 +11,7 @@
 #'
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
+#' @importFrom stats cor mad median sd setNames
 #' @export
 normalize <- function(population, variables, strata, sample, operation = "standardize", ...) {
   scale <- function(data, location, dispersion, variables) {
