a3f1166d9abc8bd627acfeb5448ca862168b8531
Shantanu Singh <shsingh@broadinstitute.org>
2016-10-17 15:08:45 -0400

    Renamed argument

--- R/variable_importance.R
+++ R/variable_importance.R
@@ -1,6 +1,6 @@
 #' Measure variable importance
 #'
-#' @param population ...
+#' @param sample ...
 #' @param variables ...
 #' @param operation ...
 #' @param ... arguments passed to variable importance operation
@@ -10,9 +10,9 @@
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
 #' @export
-variable_importance <- function(population, variables, operation = "replicate_correlation", ...) {
+variable_importance <- function(sample, variables, operation = "replicate_correlation", ...) {
   if (operation == "replicate_correlation") {
-   importance <- replicate_correlation(population, variables, ...)
+   importance <- replicate_correlation(sample, variables, ...)
   } else {
     error <- paste0("undefined operation `", operation, "'")
 
