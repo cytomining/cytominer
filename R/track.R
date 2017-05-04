33531ad3fac3f24b2df2433aa7ae1906dda2a484
Tim Becker <tbecker@broadinstitute.org>
2017-05-04 13:21:09 -0400

    added track verb including tests

--- R/track.R
+++ R/track.R
@@ -1,41 +1,30 @@
-#' Compute trajectory statistics
+#' Compute track statistics
 #'
 #' @param population ...
 #'
-#' @return trajectory statistics
+#' @return track statistics
 #'
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
 #' @export
-track <- function(population, ...) {
+#'
+track <- function(population, group_track, ...) {
   # process `population`, which is the data you get from CellProfiler
+  tracks <- add_dist(population, group_track)
+  
+  track_feature_list <- list(
+    get_track_angle(tracks),
+    get_track_chemotactic_index(tracks),
+    get_track_directionality(tracks),
+    get_track_distance(tracks),
+    get_track_dp(tracks),
+    get_track_fmi(tracks),
+    get_track_lifetime(tracks),
+    get_track_msd(tracks,t = 5),
+    get_track_sectors(tracks),
+    get_track_speed(tracks)) 
+  
+  tracks <-  Reduce(function(...) merge(..., all = TRUE, by = group_track), track_feature_list)
 
-  calculate.speed <- function(population) {
-    #  if (NROW(x) > 121) {
-    #    print(unique(x[1,c("TrackObjects_Label", "Metadata_condition", "Metadata_MATRIX", "Metadata_dateCreated")]))
-    #  }
-    track <- population %>% 
-      dplyr::select(AreaShape_Center_X, AreaShape_Center_Y, Metadata_timePoint) %>%
-      dplyr::arrange(Metadata_timePoint)
-    
-    res <- track %>%
-      dplyr::mutate(Metadata_timePoint2 =  Metadata_timePoint - 1) %>% 
-      dplyr::select(-Metadata_timePoint) %>%
-      dplyr::left_join(., track, by = c("Metadata_timePoint2" = "Metadata_timePoint")) %>%
-      na.omit(.)  %>% 
-      dplyr::mutate(speed = sqrt((AreaShape_Center_X.y - AreaShape_Center_X.x)^2 +
-                            (AreaShape_Center_Y.y - AreaShape_Center_Y.x)^2)) %>%
-      dplyr::select(-AreaShape_Center_X.y, -AreaShape_Center_X.x, -AreaShape_Center_Y.y, -AreaShape_Center_Y.x) 
-    return(res)
-  }
-  
-  population.with.speed <- population %>% 
-    dplyr::group_by(TrackObjects_Label) %>%
-    dplyr::do(calculate.speed(.)) 
-  
-  mean.speed <- population.with.speed %>% 
-    dplyr::group_by(TrackObjects_Label) %>%
-    dplyr::summarise( mean.speed = mean(speed )) 
-  
-  return(mean.speed)
+  return(tracks)
 }
--- tests/testthat/test-track.R
+++ tests/testthat/test-track.R
@@ -1,19 +1,143 @@
-context("trajectory")
+context("track")
 
-test_that("`trajectory` summarizes trajectories", {
-
-  # sample data set with two simple tracks
+test_that("`track` collapse single cell data to track objects", {
   
-  data <- tibble::data_frame(Metadata_timePoint = c(1:5),
-                             AreaShape_Center_X = c(1,5,1,5,9), 
-                             AreaShape_Center_Y = c(1,4,1,4,7), 
-                             TrackObjects_Label = c(rep(1,2),rep(2,3))
-  ) 
+  # sample data set with one simple tracks
+  # todo: add more tracks
+  data <- tibble::data_frame(
+    Metadata_timePoint = c(1:5),
+    Location_Center_X = c(1, 2, 3, 4, 5),
+    Location_Center_Y = c(1, 1, 1, 1, 1),
+    TrackObjects_Label = c(rep(1, 5))
+  )
 
-  res <- track(data)
-  expect_true(all(
-    res$mean.speed == 5
-  ))
+  distances <- tibble::data_frame(
+    TrackObjects_Distance_Traveled = c(1,1,1,1)
+  )
+  
+  track_angle <- tibble::data_frame(
+    Track_Angle = c(0)
+  )
+  
+  track_ci  <- tibble::data_frame(
+    Track_CI = c(-1)
+  )
+  
+  track_directionality  <- tibble::data_frame(
+    Track_Directionality = c(1)
+  )
+  
+  track_distance  <- tibble::data_frame(
+    Track_Distance_Traveled = c(4),
+    Track_Integrated_Distance_Traveled = c(4)
+    
+  )
+  
+  track_dp  <- tibble::data_frame(
+    Track_DP = c(3)
+  )
+  
+  track_fmi <- tibble::data_frame(
+    Track_xFMI = c(1),
+    Track_yFMI = c(0)
+  )
+  
+  track_life_time  <- tibble::data_frame(
+    Track_Length = c(as.integer(5)),
+    Track_Life_Time = c(as.integer(5)),
+    Track_One_Cell = TRUE
+  )
+  
+  track_msd  <- tibble::data_frame(
+    Track_MSD = c(0)
+  )
+  
+  track_sectors  <- tibble::data_frame(
+    Track_Positive_Sector = c(0), 
+    Track_Negative_Sector = c(1), 
+    Track_Neutral_Sector_Up = c(0), 
+    Track_Neutral_Sector_Down = c(0), 
+    Track_Sector = c(2)
+  )
+  
+  track_speed  <- tibble::data_frame(
+    Track_Speed = c(1),
+    Track_Speed_max = c(1), 
+    Track_Speed_X = c(1), 
+    Track_Speed_Y = c(0) 
+  )
+
+  track_data <- cytominer::add_dist(data,'TrackObjects_Label')
+
+  expect_equal(
+    track_data %>%
+      dplyr::select(TrackObjects_Distance_Traveled) %>%
+      na.omit(), 
+    distances
+    )
+  
+  expect_equal( 
+    track_data %>% cytominer::get_track_angle(.),
+    track_angle
+  )
+  
+  expect_equal( 
+    track_data %>% cytominer::get_track_angle(.),
+    track_angle
+  )
+  
+  expect_equal( 
+    track_data %>% 
+      cytominer::get_track_chemotactic_index(.),
+    track_ci
+  )
+  
+  expect_equal(   
+    track_data %>% 
+      cytominer::get_track_directionality(.),
+    track_directionality
+  )
+  
+  expect_equal( 
+    track_data %>% 
+      cytominer::get_track_distance(.),
+    track_distance
+  )
+  
+  expect_equal( 
+    track_data %>% 
+      cytominer::get_track_dp(.),
+    track_dp
+  )
+  
+  expect_equal( 
+    track_data %>% 
+      cytominer::get_track_fmi(.),
+    track_fmi
+  )
+  
+  expect_equal( 
+    track_data %>% 
+      cytominer::get_track_lifetime(.),
+    track_life_time
+  )
+  
+  expect_equal( 
+    track_data %>% 
+      cytominer::get_track_msd(.,2),
+    track_msd
+  )
+  
+  expect_equal( 
+    track_data %>% 
+      cytominer::get_track_sectors(),
+    track_sectors
+  )
+  
+  expect_equal( 
+    track_data %>% 
+      cytominer::get_track_speed(),
+    track_speed
+  )
+  
 })
-
-
