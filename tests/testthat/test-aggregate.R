aaefa892ba2d0df71ace2f7325f943003c0d9d7c
bethac07 <bethac07@users.noreply.github.com>
2017-08-28 07:55:13 -0400

    Add standard deviation to the list of aggregating functions allowed (#48)
    * Try adding standard deviation
    
    * Bump year
    
    * Revert year
    
    * Update aggregate.R
    
    Change to mean+sd
    
    * Update test-aggregate.R
    
    * Update test-aggregate.R

--- tests/testthat/test-aggregate.R
+++ tests/testthat/test-aggregate.R
@@ -8,19 +8,40 @@ test_that("`aggregate` aggregates data", {
       data.frame(g = "b", x = rnorm(5), y = rnorm(5))
     )
 
-  db <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+  db <- dplyr::src_sqlite(":memory:", create = T)
   
   data <- dplyr::copy_to(db, data)
 
   expect_equal(
     aggregate(population = data,
               variables = c("x", "y"),
-              strata = c("g")) %>%
+              strata = c("g"),
+              operation = "median") %>%
       dplyr::collect(),
     data %>%
       dplyr::group_by(g) %>%
-      dplyr::summarise_each_(dplyr::funs(mean), vars = c("x", "y"))
+      dplyr::summarise_at(.funs = dplyr::funs(median), .vars = c("x", "y"))
   )
 
-  DBI::dbDisconnect(db)
+  expect_equal(
+    aggregate(population = data,
+              variables = c("x", "y"),
+              strata = c("g"),
+              operation = "median") %>%
+      dplyr::collect(),
+    data %>%
+      dplyr::group_by(g) %>%
+      dplyr::summarise_at(.funs =dplyr::funs(median), .vars = c("x", "y"))
+  )
+
+  expect_equal(
+    aggregate(population = data,
+              variables = c("x", "y"),
+              strata = c("g"),
+              operation = "mean+sd") %>%
+      dplyr::collect(),
+    data %>%
+      dplyr::group_by(g) %>%
+      dplyr::summarise_at(.funs =c(dplyr::funs(mean), dplyr::funs(sd)), .vars = c("x", "y"))
+  )
 })
