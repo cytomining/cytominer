c3e9a892d020c9144fdbbe0c170cea1ffdb39f67
Tim Becker <tbecker@broadinstitute.org>
2017-08-30 10:30:54 -0400

    fix for #56

--- R/drop_na_rows.R
+++ R/drop_na_rows.R
@@ -5,25 +5,32 @@
 #'
 #' @return data without rows with NA in all variables
 #'
+#' 
+#'
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
 #' @export
 drop_na_rows <- function(population, variables) {
-
- # Coalesce() must have at least 2 arguments.
- if(length(variables) == 1)
-    variables <- c(variables, variables)
-
- population %>%
-   dplyr::filter_(.dots =
-                    sprintf("!is.null(coalesce(%s))",
-                            paste(variables, collapse = ","))
-                  )
-
- # population %>%
- #   dplyr::mutate_at(variables, dplyr::funs(is.na)) %>%
- #   dplyr::mutate(all_na = rowSums(.[variables]) == length(variables)) %>%
- #   dplyr::filter(all_na == FALSE) %>%
- #   dplyr::select(-all_na)
-
+  
+  
+  if (is.data.frame(population)) {
+    population %>%
+      rownames_to_column() %>%
+      gather_("key", "value",variables)  %>%
+      filter(!is.na(value)) %>%
+      spread(key, value) %>%
+      select(-rowname)
+  } else { 
+    
+    # Coalesce() must have at least 2 arguments.
+    if(length(variables) == 1)
+      variables <- c(variables, variables)
+    
+    population %>%
+      dplyr::filter_(.dots =
+          sprintf("!is.null(coalesce(%s))",
+            paste(variables, collapse = ","))
+      )
+  }
+  
 }
