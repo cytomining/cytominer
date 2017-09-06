8325c5227fb1df399ee93b635ecfaf9727fa41c3
Shantanu Singh <shsingh@broadinstitute.org>
2017-09-06 09:48:57 -0400

    cleanup generalized log

--- R/generalized_log.R
+++ R/generalized_log.R
@@ -1,12 +1,12 @@
-#' Generalized log transform data
-#' 
-#' Each variable is log transformed using x = log( (x + sqrt(x ^ 2 + offset ^ 2 )) / 2 ).
+#' Generalized log transform data.
 #'
-#' @param population Data frame with observation and grouping variables (metadata). 
-#' @param variables Vector of column names defining the used features. 
-#' @param offset Offset parameter for the log transformation. Default offset = 1.
+#' \code{generalized_log} transforms specified observation variables using \eqn{x = log( (x + sqrt(x ^ 2 + offset ^ 2 )) / 2 )}.
 #'
-#' @return Data frame with generalized log transformed values.
+#' @param population tbl with grouping (metadata) and observation variables.
+#' @param variables character vector specifying observation variables.
+#' @param offset offset parameter for the transformation.
+#'
+#' @return transformed data of the same class as \code{population}.
 #'
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
@@ -14,14 +14,14 @@
 #' @export
 generalized_log <- function(population, variables, offset = 1) {
   offset <- rlang::enquo(offset)
-  
+
   for (variable in variables) {
     x <- rlang::sym(variable)
-    
+
     population %<>%
       dplyr::mutate(!!x := log( ((!!x) + ((!!x) ^ 2 + (!!offset) ^ 2) ^ 0.5 ) / 2))
   }
-  
+
   population
 }
 
