6230941b64cd1f424ebad5e5d0681dea0e537832
Shantanu Singh <shsingh@broadinstitute.org>
2017-09-06 18:20:20 -0400

    cleanup normalize

--- R/normalize.R
+++ R/normalize.R
@@ -1,31 +1,33 @@
-#' Normalize a data frame based using a control group.   
+#' Normalize observation variables..
 #'
-#' @param population Data frame with observation and grouping variables (metadata). 
-#' @param variables Vector of column names defining the used features.
-#' @param strata Vector of column names used as grouping variables. 
-#' @param sample Data frame used to estimate the distribution of the control group.
-#' @param operation The following methods can be chosen to normalize the data. 1. standardize were the data is normalized using X = (X - mu) / sigma where mu is the estimated mean value of the control data and sigma  or standardize. Default value operation = "standardize".
+#' \code{normalize} normalizes observation variables based on the specified normalization method.
+#'
+#' @param population tbl with grouping (metadata) and observation variables.
+#' @param variables character vector specifying observation variables.
+#' @param strata character vector specifying grouping variables for grouping prior to normalization.
+#' @param operation optional character string specifying method for normalization. This must be one of the strings \code{"standardize"}, \code{"robustize"}.
+#' @param sample tbl containing sample that is used by normalization methods to estimate parameters. \code{sample} has same structure as \code{population}. Typically, \code{sample} corresponds to controls in the experiment.
 #' @param ... arguments passed to normalization operation
 #'
-#' @return Normalized data as data frame. 
+#' @return normalized data of the same class as \code{population}.
 #'
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
 #' @importFrom rlang :=
 #' @importFrom stats cor mad median sd setNames
-#' 
+#'
 #' @examples
 #' library(magrittr)
 #' population <- tibble::data_frame(
-#'    Metadata_group = c("control", "control","control","control",
-#'                       "experiment","experiment","experiment","experiment"),
-#'    Metadata_batch = c("a","a","b","b","a","a","b","b"),
-#'    AreaShape_Area = c(10,12,15,16,8,8,7,7)
+#'    Metadata_group = c("control", "control", "control", "control",
+#'                       "experiment", "experiment", "experiment", "experiment"),
+#'    Metadata_batch = c("a", "a", "b", "b", "a", "a", "b", "b"),
+#'    AreaShape_Area = c(10, 12, 15, 16, 8, 8, 7, 7)
 #'  )
 #' variables <- c('AreaShape_Area')
 #' strata <- c('Metadata_batch')
 #' sample <- population %>% dplyr::filter(Metadata_group == 'control')
-#' normalized = cytominer::normalize(population, variables, strata, sample, operation = "standardize")
+#' cytominer::normalize(population, variables, strata, sample, operation = "standardize")
 #' @export
 normalize <- function(population, variables, strata, sample, operation = "standardize", ...) {
   scale <- function(data, location, dispersion, variables) {
@@ -50,14 +52,14 @@ normalize <- function(population, variables, strata, sample, operation = "standa
 
       for (variable in variables) {
         x <- rlang::sym(variable)
-        
+
         m <- location[[variable]]
-        
+
         s <- dispersion[[variable]]
-        
+
         data %<>%
           dplyr::mutate(!!x := ((!!x) - m) / s )
-        
+
       }
 
       data
@@ -65,7 +67,7 @@ normalize <- function(population, variables, strata, sample, operation = "standa
   }
 
   sample_is_df <- is.data.frame(sample)
-  
+
   if (operation == "robustize") {
     location <- ifelse(sample_is_df,
                        dplyr::funs(median(., na.rm = TRUE)),
@@ -74,12 +76,12 @@ normalize <- function(population, variables, strata, sample, operation = "standa
     dispersion <- ifelse(sample_is_df,
                          dplyr::funs(mad(., na.rm = TRUE)),
                          dplyr::funs(mad))
-    
+
   } else if (operation == "standardize") {
     location <- ifelse(sample_is_df,
                        dplyr::funs(mean(., na.rm = TRUE)),
                        dplyr::funs(mean))
-    
+
     dispersion <- ifelse(sample_is_df,
                          dplyr::funs(sd(., na.rm = TRUE)),
                          dplyr::funs(sd))
