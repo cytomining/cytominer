28f464be74a11e22f97682b6f75d24a634b31165
Shantanu Singh <shsingh@broadinstitute.org>
2017-09-06 20:58:38 -0400

    cleanup drop_na_rows

--- R/drop_na_rows.R
+++ R/drop_na_rows.R
@@ -1,20 +1,22 @@
-#' Drop rows that are NA in all variables
+#' Drop rows that are \code{NA} in all variables.
 #'
-#' @param population Data frame with observation and grouping variables (metadata).
-#' @param variables Vector of column names defining the used features.
+#' \code{drop_na_rows} drops rows that are \code{NA} in all variables.
 #'
-#' @return data without rows with NA in all variables
+#' @param population tbl with grouping (metadata) and observation variables.
+#' @param variables character vector specifying observation variables.
+#'
+#' @return \code{population} without rows that have \code{NA} in all variables.
 #'
 #' @examples
 #'  population <- tibble::data_frame(
-#'    Metadata_group = c("control", "control","control","control",
-#'                       "experiment","experiment","experiment","experiment"),
-#'    Metadata_batch = c("a","a","b","b","a","a","b","b"),
-#'    AreaShape_Area = c(10,12,15,16,8,8,7,7),
-#'    AreaShape_length = c(2,3,NA,NA,4,5,1,5)
+#'    Metadata_group = c("control", "control", "control", "control",
+#'                       "experiment", "experiment", "experiment", "experiment"),
+#'    Metadata_batch = c("a", "a", "b", "b", "a", "a", "b", "b"),
+#'    AreaShape_Area = c(10, 12, NA, 16, 8, 8, 7, 7),
+#'    AreaShape_Length = c(2, 3, NA, NA, 4, 5, 1, 5)
 #'  )
-#' variables <- c('AreaShape_Area','AreaShape_length')
-#' na_per_row = drop_na_rows(population, variables)
+#' variables <- c('AreaShape_Area','AreaShape_Length')
+#' drop_na_rows(population, variables)
 #' 
 #'
 #' @importFrom magrittr %>%
@@ -25,10 +27,11 @@ drop_na_rows <- function(population, variables) {
   if (is.data.frame(population)) {
     population %>%
       tibble::rownames_to_column(., var = "rowname_temp") %>%
-      tidyr::gather_("key", "value",variables)  %>%
+      tidyr::gather_("key", "value", variables)  %>%
       dplyr::filter(!is.na(value)) %>%
       tidyr::spread(key, value) %>%
       dplyr::select(-rowname_temp)
+    
   } else { 
     
     # Coalesce() must have at least 2 arguments.
