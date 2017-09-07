5e1cd32c03615a1030ed7c9142d1b3004bc251b6
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-09-07 13:05:37 -0400

    testing the consistency of the subpopulation extraction

---
+++ tests/testthat/test-extract_subpopulations.R
@@ -0,0 +1,69 @@
+context("extract_subpopulations")
+
+test_that("`extract_subpopulations` extracts and assigns each point to a subpopulation", {
+  set.seed(24)
+
+  k <- 6  
+  data_trt <- data.frame(matrix(runif(1000), 100, 10),
+                         Metadata_id = 1:100)
+
+  data_ctrl <- data.frame(matrix(runif(2000), 200, 10),
+                         Metadata_id = 1:200)
+  
+  feats <- colnames(data_trt)
+  feats <- feats[which(!stringr::str_detect(feats, "Metadata_"))]
+  
+  subpops <- extract_subpopulations(population_treatment = data_trt, 
+                         population_control = data_ctrl, 
+                         feats = feats, 
+                         k = k)  
+  
+  cluster_assign <- function(data, centers, feats, k) {
+    dist_to_clusters <- as.matrix(dist(rbind(data[,feats], 
+                                             centers)))[1:NROW(data),
+                                                        (NROW(data) + 1):(NROW(data) + k)]
+    clusters <- t(apply(dist_to_clusters, 1, 
+                            function(x) c(min(x),
+                                          which.min(x))))
+    colnames(clusters) <- c("Metadata_dist_to_cluster", "Metadata_Cluster")
+    clusters <- dplyr::as_tibble(clusters) 
+    clusters$Metadata_Cluster <- as.integer(clusters$Metadata_Cluster)
+    return(clusters)
+  }
+  
+  trt_clusters <- cluster_assign(data = data_trt, 
+                                 centers = subpops$subpop_centers, 
+                                 feats = feats, 
+                                 k = k)
+
+  ctrl_clusters <- cluster_assign(data = data_ctrl, 
+                                 centers = subpops$subpop_centers, 
+                                 feats = feats, 
+                                 k = k)
+  
+  ## test whether the cluster assignment and distance to the clusters 
+  ## are consistent with the returned cluster centers
+  
+  expect_equal(
+    subpops$treatment_clusters[, c("Metadata_dist_to_cluster", 
+                                              "Metadata_Cluster")],
+    trt_clusters
+  )
+  
+  expect_equal(
+    subpops$ctrl_clusters[, c("Metadata_dist_to_cluster", 
+                                   "Metadata_Cluster")],
+    ctrl_clusters
+  )
+  
+  ## test if the summation of cluster proportions are equal to one
+  expect_equal(
+    subpops$subpop_profiles %>% 
+      dplyr::select(-Metadata_Cluster) %>%
+      tidyr::gather(key = "Metadata_Type", value = "freq") %>%
+      dplyr::group_by(Metadata_Type) %>%
+      dplyr::summarise(tot.freq = sum(freq)) %>%
+      dplyr::select(tot.freq),
+    dplyr::tibble(tot.freq = c(1, 1))
+  )
+})
