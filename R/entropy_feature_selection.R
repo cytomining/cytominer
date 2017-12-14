01a74bbf25a56acff4659b82f0955948dcb06380
Mohammad Hossein Rohban <mrohban@broadinstitute.org>
2017-12-14 10:31:19 -0500

    modified doc. for the R file

--- R/entropy_feature_selection.R
+++ R/entropy_feature_selection.R
@@ -1,12 +1,12 @@
-#' entropy based feature selection
+#' feature selection based on the entropy of data singular values
 #'
-#' @param population named tbl containing data, where columns and rows correspond to features and samples, respectively. Column names are assumed to be feature names.
+#' @param population named tbl containing data, where columns and rows correspond to features (and/or metadata) and samples, respectively. Column names are assumed to be feature or metadata names.
 #' @param variables vector containing the names of numerical variables (or features) in the population.
 #' @param n_feature integer specifying number of features to be selected
 #'
 #' @importFrom magrittr %>%
 #'
-#' @return vector containing name of the features sorted based on their SVD score. Higher score means better.
+#' @return vector containing name of the features sorted based on their score, and the actual score values. Higher score means more informative feature.
 #'
 #' @export
 entropy_feature_selection <- function(population, variables, n_feature) {
@@ -15,6 +15,7 @@ entropy_feature_selection <- function(population, variables, n_feature) {
     dplyr::select(dplyr::one_of(variables)) %>%
     as.matrix()
 
+  # working with the matrix inner product; as it would be computationally more efficient for large number of samples
   feat_inner_prods <- crossprod(population_data, population_data)
 
   entropy_score <- score_features_sv_entropy(feat_inner_prods)
