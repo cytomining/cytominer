9ac6eaa71ab1c477bf2e6c488c69aa766df4599b
Shantanu Singh <shsingh@broadinstitute.org>
2021-04-01 03:46:19 -0400

    Update description

---
+++ R/cytominer-package.R
@@ -0,0 +1,8 @@
+#' @description
+#'
+#' `cytominer` is a suite of common functions used to process high-dimensional
+#' readouts from high-throughput cell experiments.
+#'
+#' @keywords internal
+#' @import rlang
+"_PACKAGE"
