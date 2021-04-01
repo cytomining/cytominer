761ea047327c6587f76c56608632bb373d398241
Shantanu Singh <shsingh@broadinstitute.org>
2021-04-01 06:45:06 -0400

    test with data.frame

--- tests/testthat/test-drop_na_columns.R
+++ tests/testthat/test-drop_na_columns.R
@@ -3,6 +3,22 @@ context("drop_na_columns")
 test_that("`drop_na_columns` removes columns have only NAs", {
   data <- data.frame(x = rnorm(5), y = NA)
 
+  expect_equal(
+    drop_na_columns(
+      population = data,
+      variables = c("x", "y")
+    ),
+    c("y")
+  )
+
+  expect_equal(
+    drop_na_columns(
+      population = data,
+      variables = c("x")
+    ),
+    character(0)
+  )
+
   db <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
 
   data <- dplyr::copy_to(db, data)
