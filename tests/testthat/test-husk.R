169fa86c6653c94354a5b3f348124971c8fa98d9
Shantanu Singh <shsingh@broadinstitute.org>
2021-04-05 12:54:25 -0400

    remove test file, move to husk

--- tests/testthat/test-husk.R
+++ tests/testthat/test-husk.R
@@ -1,3 +1,92 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context("husk")
+
+test_that("`husk` husks data", {
+
+  n_dim <- 15
+  n_points <- 10
+  data <- matrix(rnorm(n_points * n_dim), n_points, n_dim)
+  data <- data + abs(matrix(rnorm(n_points * n_dim), n_points, n_dim)) * 50
+  data <- as.data.frame(data)
+
+  variables <- names(data)
+
+  expect_equal(
+    spherize(
+      population = data,
+      variables = variables,
+      sample = data,
+      regularization_param = 0
+    ) %>%
+      cov() %>%
+      as.matrix() %>%
+      unname(),
+    diag(rep(1, n_dim)),
+    tolerance = 10 ^ -10
+  )
+
 })
+#
+# test_that("`spherize` uses svd consistently", {
+#
+#   data <- matrix(rnorm(10 * 100), 10, 100)
+#   data <- data + abs(matrix(rnorm(10 * 100), 10, 100)) * 10
+#   data <- as.data.frame(data)
+#
+#   variables <- names(data)
+#
+#   regularization_param  <- 0
+#
+#   spherize_df <-
+#     spherize(
+#       population = data,
+#       variables = variables,
+#       sample = data,
+#       regularization_param = 0
+#     )
+#
+#   spherize_test <- function(data, regularization_param)  {
+#
+#     sample_mean <- colMeans(sample_data)
+#
+#     sample_cov <- cov(data)
+#
+#     n <- nrow()
+#
+#     # eigen decomposition \Sigma = E * \Lambda * E'
+#     eig_decomp <- eigen(data)
+#
+#     E_t <- t(eig_decomp$vectors)
+#
+#     Lambda <- eig_decomp$values
+#
+#     # compute sphering transformation, which is {\Lambda + \epsilon}^.5 x E'
+#     W <- diag((Lambda + regularization_param)^-0.5) %*% E_t
+#
+#     # apply sphering transformation, which is (X - \mu) * W'
+#     transformed_data <- sweep(data, 2, sample_mean) %*% t(W)
+#
+#   }
+#
+#   # ---------------------------------------------------------------------------
+#   # ---------------------------------------------------------------------------
+#
+#   df <- data.frame(svd = Lambda[1:length(Lambda)-1], eig = xLambda[1:length(Lambda)-1])
+#
+#   lm(df, formula = svd ~ eig)
+#
+#   xx <- cor(xtransformed_population_data[,1:(n-1)], transformed_population_data[,1:(n-1)])
+#
+#   plot(diag(xx))
+#
+#
+#   expect_equal(
+#     spherize(
+#       population = data,
+#       variables = c("x", "y", "z"),
+#       sample = data,
+#       regularization_param = 0
+#     )
+#   )
+#
+#
+# })
