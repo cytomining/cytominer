cbb48439f8babcf3145b7d7c99d110fdc443d731
Shantanu Singh <shsingh@broadinstitute.org>
2021-04-06 00:39:00 -0400

    rethinking husk

--- R/husk.R
+++ R/husk.R
@@ -10,9 +10,8 @@
 #'   Typically, \code{sample} corresponds to controls in the experiment.
 #' @param regularization_param optional parameter used in husking to offset
 #'   eigenvalues to avoid division by zero. Default is \code{1}.
-#' @param husk optional boolean specifying whether to husk the signal instead of
-#'   just scaling it down.
-#'   Default is \code{TRUE}.
+#' @param remove_signal optional boolean specifying whether to husk the signal
+#'   instead of only scaling it down. Default is \code{TRUE}.
 #' @param remove_outliers optional boolean specifying whether to remove
 #'   outliers. Default is \code{TRUE}.
 #'
@@ -25,10 +24,10 @@
 #'   Texture_DNA = c(5, 2, 43, 13)
 #' )
 #' variables <- c("Intensity_DNA", "Texture_DNA")
-#' husk(population, variables, population, 1, husk = TRUE)
-#' husk(population, variables, population, 1e-5, husk = TRUE)
-#' husk(population, variables, population, 1, husk = FALSE)
-#' husk(population, variables, population, 1e-5, husk = FALSE)
+#' husk(population, variables, population, 1, remove_signal = TRUE)
+#' husk(population, variables, population, 1e-5, remove_signal = TRUE)
+#' husk(population, variables, population, 1, remove_signal = FALSE)
+#' husk(population, variables, population, 1e-5, remove_signal = FALSE)
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
 #' @importFrom rlang :=
@@ -38,8 +37,8 @@ husk <-
   function(population,
            variables,
            sample,
-           regularization_param = 1,
-           husk = TRUE,
+           regularization_param = 1e-6,
+           remove_signal = TRUE,
            remove_outliers = TRUE) {
     # -------------------------
     # Get the sample matrix
@@ -111,62 +110,31 @@ husk <-
     #
     # Get the full V, not just row space, because we need its null space as well
     # (for n < d). Note that we don't need U, just S and V.
-    # Scale the singular value by dividing by sqrt(n-1) to get the s.d.
-    # of the corresponding PC
 
     xsvd <- svd(X, nu = 0, nv = d)
     V <- xsvd$v
-    S <-
-      xsvd$d / sqrt(n - 1) # divide by sqrt(n-1) so as to get the s.d.
 
-    # Recap: S is the s.d. of the PC's
+    # -------------------------
+    # Get s.d. of PCs from SVD
+    # -------------------------
+    #
+    # Scale the singular value by dividing by sqrt(n-1) to get the s.d.
+    # of the corresponding PC
+    # Recap:
     # all.equal(
     #   prcomp(X)$sdev,
     #   svd(scale(X, center = TRUE, scale = FALSE))$d / sqrt(n-1)
     # )
 
+    S <- xsvd$d / sqrt(n - 1)
+
     # -------------------------
     # Get projection matrix
-    # TODO:
-    #   - Ponder the rationale overall
     # -------------------------
 
-    # Find the first s.d.^2 that is less than `husk_threshold`
-    # (default = max(1, d/n))
-    # We will later (optionally) do one or both of the following
-    #   - set the s.d. to `sqrt(husk_threshold)` for all PCs with s.d.^2 <
-    #     `husk_threshold`
-    #   - drop all PCs that have s.d.^2's >= `husk_threshold`
-    # TODO:
-    #   - Ponder the rationale of choosing max(1, d/n) as the default.
-    #     It might be flawed!
-    #
-    #     Update: turns out that the threshold should likely be max(1, d/n)
-    #     based on purely empirical observations.
-    #
-    #     Former rationale:
-    #
-    #     The rationale is that when PCA is performed on X with unit s.d., (or,
-    #     equivalently, when PCA is performed on the correlation matrix instead
-    #     of the covariance matrix), we assume that PCs with > 1 s.d. definitely
-    #     contain signal (and should thus be (optionally) dropped, even if they
-    #     are scaled down to unit s.d.). We should husk the signal, and keep
-    #     the (whitened) noise.
-    #
-
-    husk_threshold <- max(1, d/n)
-
-    # Note q is NA if *no* s.d. is less than `husk_threshold`
-    q <- which(S^2 < husk_threshold)[1]
-    futile.logger::flog.debug(glue::glue("{qx} PCs have s.d. > {husk_threshold}",
-      qx = ifelse(is.na(q), 0, q)
-    ))
-
-    # - Set the s.d. of the vectors of the null space to the smallest s.d.
-    #   (when n <= d; there is no null space otherwise)
-    # - Add a regularizer
-    # TODO :
-    #   - Ponder the rationale for padding
+    # Set the s.d. of the vectors of the null space to the smallest s.d.
+    # (when n <= d; there is no null space otherwise), and then add a
+    # regularizer
 
     if (n <= d) {
       Sr <- c(S[1:r], rep(S[r], d - r))
@@ -174,33 +142,39 @@ husk <-
       Sr <- S
     }
 
+    # Regularize
     Sr <- Sr + regularization_param
 
-    # I considered this alternative to regularization but abandoned it:
-    # - Set the s.d. to `sqrt(husk_threshold)` for all the basis vectors of
-    #   the null (when n <= d; there is no null space otherwise)
-    # - Set the s.d. to `sqrt(husk_threshold)` for all PCs with s.d. < 1
-    # - Do not add a regularizer
-
-    #  if (!is.na(q)) {
-    #    if (n <= d) {
-    #      Sr <- c(S[1:(q - 1)], rep(1, d - q + 1))
-    #    } else {
-    #      Sr <- S
-    #      Sr[q:d] <- sqrt(husk_threshold)
-    #    }
-    #  }
-
+    # (See comments at the end of the function regularization alternatives)
 
     proj <- diag(1 / Sr) %*% t(V)
 
     # -------------------------
+    # Find number of PCs to retain to keep the signal
+    # (but we actually later want to throw them out, not keep them)
+    # -------------------------
+    # This is a whole subfield in itself, and there are many ways of doing it.
+    # But we can take a lazy approach for now and just trim the s.d. that are
+    # "outliers"
+
+    outlier_s <- boxplot(S^2, plot = FALSE)$out
+
+    if(length(outlier_s) == 0) {
+      q <- 0
+    } else {
+      q <- which(S^2 < min(outlier_s))[1]
+    }
+
+    futile.logger::flog.info(glue::glue("There are {q} PCs with signal."))
+
+    # -------------------------
     # Optionally trim the projection matrix
     # -------------------------
+    #
     # Rationale: PCs with signal should be removed
 
-    if (husk) {
-      proj <- proj[q:d, ] # husk the signal, keep the white noise
+    if (remove_signal) {
+      proj <- proj[(q+1):d, ] # husk the signal, keep the white noise
     }
 
     # -------------------------
@@ -243,3 +217,18 @@ husk <-
 
     husked
   }
+
+# I considered this alternative to regularization but abandoned it:
+# - Set the s.d. to `sqrt(husk_threshold)` for all the basis vectors of
+#   the null (when n <= d; there is no null space otherwise)
+# - Set the s.d. to `sqrt(husk_threshold)` for all PCs with s.d. < 1
+# - Do not add a regularizer
+
+#  if (!is.na(q)) {
+#    if (n <= d) {
+#      Sr <- c(S[1:(q - 1)], rep(1, d - q + 1))
+#    } else {
+#      Sr <- S
+#      Sr[q:d] <- sqrt(husk_threshold)
+#    }
+#  }
